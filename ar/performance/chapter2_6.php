0c2fbb635106d451af899c8583418c58cd186f29
Abidul <abd_rmdn@yahoo.com>
2015-04-22 12:59:30 +0800

    adding initial files for performance file..

---
+++ ar/performance/chapter2_6.php
@@ -0,0 +1,20 @@
+<?php
+
+    include("../inc/header.php");
+
+?>
+
+
+<div idclass="spacer s0"></div>
+		
+<div id="movingSectionContainer">
+    <div class="section1 section">
+    </div>
+</div>
+
+
+<?php
+    include("../inc/footer.php");
+?>
+<link rel="stylesheet" href="../assets/css/ch2/chapter2_6.css">
+<script src="../assets/js/ch2/chapter2_6.js"></script>
--- ar/performance/header_footer_template.php
+++ ar/performance/header_footer_template.php
@@ -4,7 +4,6 @@
 
 ?>
 
-
 <?php
     include("../inc/footer.php");
 ?>
