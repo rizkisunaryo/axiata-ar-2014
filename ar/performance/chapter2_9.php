dd1598df5f8e6a8d1469995679655ce4b78c8477
nratifainah <nurul@carrottop.biz>
2015-04-23 19:05:03 +0800

    add chapter 2_9 and chapter 2_8(rotated label)

---
+++ ar/performance/chapter2_9.php
@@ -0,0 +1,187 @@
+<?php
+    include("../inc/header.php");
+?>
+    <div class="rel section">
+        <div class="axiataBold">
+            <p class="text__color--lightgrey font25">Investor <span class="block text__color--lightred font31">Relations</p>
+            <p class="text__color--lightred font20">Axiata’s investor relations is commited to handling inquiries from the investing community which includes equity analysts, fund managers and institutional shareholders, as well as others who are interested in undertaking an investment in the Company.</p>
+        </div>
+        <div class="row">
+            <div class="text__color--darkgrey font20">Equity Research Coverage</div>
+            <div>
+                <div class="col col3">
+                    <ul class="removeMargin removeMargin--add-start-margin equity-list">
+                        <li>Affin Securities</li>
+                        <li>AmResearch</li>
+                        <li>Barclays</li>
+                        <li>BIMB Securities</li>
+                        <li>BNP Paribas</li>
+                        <li>BofA - Merrill Lynch</li>
+                        <li>CIMB</li>
+                        <li>Citi</li>
+                        <li>CLSA</li>
+                        <li>Credit Suisse</li>
+                        <li>DBS Vickers</li>
+                    </ul>
+                </div>
+                <div class="col col3">
+                    <ul class="removeMargin removeMargin--add-start-margin equity-list">
+                        <li>Deutsche Bank</li>
+                        <li>Goldman Sachs</li>
+                        <li>Hong Leong Investment Bank</li>
+                        <li>HSBC</li>
+                        <li>JF Apex Securities</li>
+                        <li>JPMorgan</li>
+                        <li>KAF Seagroatt & Campbell</li>
+                        <li>Kenanga Investment Bank</li>
+                        <li>Macquarie</li>
+                        <li>Maybank Kim Eng</li>
+
+                    </ul>
+                </div>
+                <div class="col col3">
+                    <ul class="removeMargin removeMargin--add-start-margin equity-list">
+                        <li>MIDF</li>
+                        <li>Morgan Stanley</li>
+                        <li>New Street Research</li>
+                        <li>Nomura</li>
+                        <li>Public Investment Bank</li>
+                        <li>RHB Research Institute</li>
+                        <li>Standard Chartered</li>
+                        <li>TA Securities</li>
+                        <li>UBS</li>
+                        <li>UOB Kay Hian</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <p class="text__color--lightred axiataBold font20">Conferences and Non-Deal Roadshows</p>
+            <div class="row text__color--darkgrey">
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="font20">Conferences</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Asian Investments <span class="block"> Conference Credit Suisse</div>
+                        <div class="font16">Asian Investments Conference <span class="block">Credit Suisse</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Global Emerging Markets <span class="block"> Conference Deutsche Bank</div>
+                        <div class="font16">New York <span class="block">3-4 Sept</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class=" divider-dash"></div>
+                </div>
+
+            </div>
+            <div class="row text__color--darkgrey">
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Investors’ Forum CLSA</div>
+                        <div class="font16">Hong Kong <span class="block">16-17 Sep</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Nomura Investment Forum</div>
+                        <div class="font16">Tokyo <span class="block">2-3 Dec</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Invest Malaysia <span class="block"> Bursa Malaysia & Maybank</div>
+                        <div class="font16">Kuala Lumpur <span class="block">13 Jun</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class=" divider-dash"></div>
+                </div>
+
+            </div>
+             <div class="row text__color--darkgrey">
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">ASEAN Forum JO Morgan</div>
+                        <div class="font16">London <span class="block">8-9 Sep</div>
+                    </div>
+                </div>
+                <div class="col col66">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Asia Pacific Summit Conference Morgan Stanley</div>
+                        <div class="font16">Singapore <span class="block"> 12 Nov</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class=" divider-dash"></div>
+                </div>
+
+            </div>
+             <div class="row text__color--darkgrey">
+                <div class="font20">Non-Deal Roadshow</div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Macquarie <span class="block"> Edinburgh, London</div>
+                        <div class="font16">27-28 Feb</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Goldman Sachs <span class="block"> Hong Kong, Singapore</div>
+                        <div class="font16">6-7 Mar</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Maybank <span class="block"> Kuala Lumpur</div>
+                        <div class="font16">26 Nov</div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class=" divider-dash"></div>
+                </div>
+
+            </div>
+            <div class="row text__color--darkgrey">
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">CIMB <span class="block"> Boston, New York</div>
+                        <div class="font16">3-4 Mar</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="axiataBold font15">Credit Suisse <span class="block"> Eidenburgh</div>
+                        <div class="font16">5 Sept</div>
+                    </div>
+                </div>
+                <div class="col col3">
+                    <div class="conference-wrap">
+                        <div class="box-wrap bg__color--purple text__color--white">
+                            <div class="axiataBold font15">Investor Relations Contact:-</div>
+                            <div class="font15">Clare Chin</div>
+                            <div class="font15">Head, Investor Relations</div>
+                            <div class="font15">Tel: +603 2263 8817</div>
+                            <div class="font15">Fax: +603 2263 7755</div>
+                            <div class="font15">Email: ir@axiata.com</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+         <div class="row">
+            <p class="axiataBold font15 text__color--lightred">Read More</p>
+        </div>
+    </div>
+</div>
+
+<?php
+    include("../inc/footer.php");
+?>
