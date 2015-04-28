15b1ed1fd5f886ff520e76cd4643d6689a933f1c
Abidul <abd_rmdn@yahoo.com>
2015-04-28 18:40:46 +0800

    risk_managment done

--- sr/Risk_Management/risk_management.php
+++ sr/Risk_Management/risk_management.php
@@ -4,175 +4,93 @@
 
 ?>
 
-<div class="section_container">
-    <div class="section1 section">
-        <div class="title">
-            <div class="small_txt">Stakeholder</div>
-            <div class="big_txt">Engagement</div>
+<div class="section_container book">
+    <div class="section1 section grey_txt">
+        <div class="main_title">
+            <div class="med_txt grey_txt bold">Risk</div>
+            <div class="big_txt red_txt bold">Management</div>
         </div>
-        <div class="content">
-            <div class="paragraph1 paragraph">
-                Our engagement with stakeholders is intrinsic to the way we do business. In line with our commitment as a telecommunications provider, we strive to understand the expectations of our stakeholders. In our attempt to determine the issues most material to the Group, we are guided by the inclusivity, materiality and responsiveness as defined by the AA1000 Principle Standard.
+        <div class="paragraph grey_txt small_txt"> 
+            The Board is responsible and accountable for maintaining and ensuring a sound system of risk management and internal control practices to safeguard shareholders’ investments, stakeholders’ interest as well as the Group’s assets. Such systems cover not only financial controls but also operational and compliance controls.
+            <br>
+            <br>The Board has an on-going process in place for identifying, evaluating, monitoring and managing significant risks that may affect the achievement of the Group’s business objectives. The Board is assisted by the Group Risk Management Committee (GRMC), consisting of all members of Axiata Group Senior Leadership Team (SLT), in ensuring that the implementation of the approved policies, manuals and procedures on risks and controls are in place to mitigate key risk areas which have been identified and assessed by the risk owners.
+            <br>
+            <br>The Axiata Enterprise Risk Management (ERM) Framework, benchmarked against ISO31000:20093 is used as a standardised approach in identifying, assessing, reporting and monitoring risks. To ensure consistency, the framework is adopted by all risk management teams across all OpCos.
+            <br>
+            <br>Risk assessment involves the identification and analysis of the key risks based on certain criteria which takes into account its effect and likelihood. Appropriate mitigation strategies are then developed, taking into consideration the effectiveness of existing controls.
+            <br>
+            <br>The following are the key risk areas for the Axiata Group:
+        </div>
+        
+        <div class="row ">
+            <div class="col col2">
+                <li><span class="fa fa-play red_txt"></span>Sustainability of Long-Term Business Strategy</li>
+                <li><span class="fa fa-play red_txt"></span>Adverse Regulatory Developments</li>
+                <li><span class="fa fa-play red_txt"></span>Competitive Risk</li>
+                <li><span class="fa fa-play red_txt"></span>Changes in Customer Behaviour</li>
+                <li><span class="fa fa-play red_txt"></span>Obsolescence of Existing Technology Over Time</li>
             </div>
-            <div class="paragraph2 paragraph">
-                We constantly engage with our full range of stakeholders in multiple ways. Our goal is to see that their feedback helps us shape the ways we implement our sustainability processes within the Group. Each stakeholder has an important role to play in improving our sustainability operations in the different facets of our business.  In looking at materiality, we engage with those divisions within the Group that in turn engage with each relevant stakeholder.
+            <div class="col col2" style="margin-left: 22px;">
+                <li><span class="fa fa-play red_txt"></span>Treasury and Funding Risks</li>
+                <li><span class="fa fa-play red_txt"></span>Systems and Network Failure</li>
+                <li><span class="fa fa-play red_txt"></span>Leadership Competency Gaps and Loss of Key Personnel</li>
+                <li><span class="fa fa-play red_txt"></span>Management of Large Scale Regional Projects</li>
+                <li><span class="fa fa-play red_txt"></span>Risks Related to Our Investments</li>
+
             </div>
-            <div class="paragraph3 paragraph">
-                At Axiata, we fully adhere to the principles of economic, environmental and social sustainability as they complement our vision to deliver optimum value to our stakeholders. We know that quality engagement with our stakeholders helps us to address and respond to their changing expectations and needs. We engage with our stakeholders in many ways, through surveys, websites, social media, market research, briefings and meetings. 
-            </div>
-            <div class="paragraph4 paragraph">
-                The Group's current stakeholder engagement practice is provided in the table below:
-            </div>
-            <div class="table">
-                <div class="header">
-                    <div class="tr">
-                                                                         
-                        <div class="td">Stakeholder Group</div>
-                        <div class="td">Types of Engagement</div>
-                        <div class="td">Frequency</div>
-                        <div class="td">Focus Areas</div>
-                    </div>
-                </div>
-                <div class="body"> 
-                    <div class="tr">
-                        <div class="td"><span> Customers </span><img src="../assets/img/tr1_people.png"/> </div>
-                        <div class="td"> <li>Customer Satisfaction via Net Promoter Score (NPS)</div>
-                        <div class="td">Annually</div>
-                        <div class="td">Customer experience, product affordability and network availability</div>
-                    </div>
-                    <div class="tr">
-                        <div class="td"><span> Employees </span><img src="../assets/img/Vector-Smart-Object1-copy-10.png"/> </div>
-                        <div class="td"><li>Employee Engagement Survey (EES)
-                            by Towers Watson (an independent
-                            research agency)</li>
-                        <li>	Town hall meetings</li>
-                        </div>
-                        <div class="td">Annually <br>Quarterly</div>
-                        <div class="td">Career development, employee feedback, employee communication</div>
-                    </div>
-                    <div class="tr">
-                        <div class="td"><span> Shareholders </span><img src="../assets/img/stake_holders_table_tr.png"/> </div>
-                        <div class="td"><li>	Annual General Meeting (AGM) </li>
-                            <li>	Investor Roadshows</li>
-                            <li>	Meetings with the Top 100 investors</li>
-                            <li>	Analyst Meetings</li>
-                        </div>
-                        <div class="td">Ongoing</div>
-                        <div class="td">Business performance and strategy, social responsibility initiatives</div>
-                    </div>
-                    <div class="tr">
-                        <div class="td"><span> Regulators and Government Authorities </span><img src="../assets/img/regulators_table_tr.png"/> </div>
-                        <div class="td"><li>	Compliance with the Malaysian Communications and Multimedia Commission (MCMC)</li>
-                            <li>Compliance with Bursa Malaysia</li>
-                        </div>
-                        <div class="td">Ongoing</div>
-                        <div class="td">Information security, PDPA, pricing, tax issues</div>
-                    </div>
-                    <div class="tr">
-                        <div class="td"><span> Suppliers </span><img src="../assets/img/suppliers_table_tr.png"/> </div>
-                        <div class="td"><li>Request for Proposals (RFP)</li>
-                            <li>	Supplier Code of Conduct</li>
-                            <li>	Vendor Survey</li>
-                        </div>
-                        <div class="td">Ongoing</div>
-                        <div class="td">Payment practices, delivery and definition of vendor performance </div>
-                    </div>
-                    <div class="tr">
-                        <div class="td"><span> Media </span><img src="../assets/img/media_table_tr.png"/> </div>
-                        <div class="td"><li>	Media Survey</li>
-                            <li>	Media Releases / Conferences</li>
-                        </div>
-                        <div class="td">Annually <br>Quarterly</div>
-                        <div class="td">Company performance, strategy, future business aspirations</div>
-                    </div>
-                    <div class="tr">
-                        <div class="td"><span> Community </span><img src="../assets/img/community_table_tr.png"/> </div>
-                        <div class="td"><li>	AYTP</li>
-                            <li>Axiata Sustainability Programmes</li>
-                        </div>
-                        <div class="td">Ongoing</div>
-                        <div class="td">Social requirements and specific feedback on programmes</div>
-                    </div>
-                </div>
-            </div>
-<!--           end of table  -->
-            <div class="paragraph4 paragraph">
-                Stakeholder perceptions from each of the above engagements have been presented, discussed and acted upon internally within the OpCos and at the Axiata Corporate Centre. Since we are at a stage of defining the future of our industry and shaping it towards future developments, these stakeholder comments are invaluable to the Group.
-            </div>
-            <div class="paragraph_title">
-            Materiality
-            </div>
-            <div class="paragraph5 paragraph">
-                We define material issues based on whether they have an impact to our business in either economic, social or environmental aspects and their relevance to our stakeholders across the Group. 
-                <br>
-                <br>Identifying Key Issues
-                <br>Our process to identify key material issues is based on the Reporting Principle and Guidance for Defining Content in the GRI G4 Sustainability Reporting framework. The sources used to identify the key issues that are material to the Axiata Group and our stakeholders in the reporting period are depicted in the table below.
-                <br>
-                <br>The key issues were then prioritised and plotted on a materiality matrix using the external and internal parameters as shown below.
-            </div>
-            
-            <div class="row">
-                <div class="col col2" style=" margin-right: 98px; ">
-                        <div class="title_card">External Sources</div>
-                        <div class="desc_card">
-                            <div class="title">Industry perception</div>
-                            <div class="desc">
-                                We conducted a benchmarking study comparing the sustainability performance of the top 20 global telecommunications companies4 in 2014. A detailed review was carried out looking into publicly available information on the material issues of our peers. 
-                            </div>
-                        </div>
-                        <div class="desc_card">
-                            <div class="title">Regulatory Perception</div>
-                            <div class="desc">
-                                We reviewed the feedback and perception of our regulators across the region. We have looked into areas our regulators stress and request us to work on. We have also taken into consideration the emphasis made by regulatory bodies not directly related to the industry as well. 
-                            </div>
-                        </div>
-                        <div class="desc_card">
-                            <div class="title">Our Stakeholder Perception</div>
-                            <div class="desc">
-                                Discussions with internal divisions engaging with our key stakeholders were conducted. Perceptions of each stakeholder group on sustainability were taken into account. The stakeholder groups we engaged with were employees, customers, the Board of Directors, shareholders, investors and suppliers. 
-                            </div>
-                        </div>
-                </div>        
-                <div class="col col2" >
-                        <div class="title_card">Internal Sources</div>
-                        <div class="desc_card">
-                            <div class="title">Group KPI</div>
-                            <div class="desc">
-                                We reviewed the Group’s key  performance indicators and their relevance to sustainability issues.
-                            </div>
-                        </div>
-                        <div class="desc_card">
-                            <div class="title">Group Policy</div>
-                            <div class="desc">
-                                We reviewed the coverage of the Group’s policies in relation to sustainability.
-                            </div>
-                        </div>
-                        <div class="desc_card">
-                            <div class="title">Top Management Perception</div>
-                            <div class="desc">
-                                We considered the frequency of each of the sustainability issues or relevant topics taken up in Senior Management Group meetings and the correlation to the company’s long-term/short-term strategy.
-                            </div>
-                        </div>
-                        <div class="desc_card">
-                            <div class="title">Risk Management Matrix</div>
-                            <div class="desc">
-                                We did an assessment of the Group’s Risk Management Matrix and the relationship of key risks across the group to the sustainability issues.
-                            </div>
-                        </div>    
-                </div>        
-            </div>
-            <div class="foot_note">
-                4 https://gsmaintelligence.com/research/2014/09/operator-group-ranking-q2-2014/444/
-            </div>
+        </div>
+        <div class="paragraph small_txt grey_txt">
+            The Axiata Group’s enterprise risk management process is depicted in the next page.
+
+            <div class="tiny_txt foot_note">3 http://www.iso.org/iso/home/standards/iso31000.html</div>
         </div>
     </div>
+    <div class="diagramme_cover diagram_img">
+        <div class="section ">
+        <div class="">
+            <!-- <div  class="diagram_img" ></div> -->
+            <div class="title white_txt bold">
+                <div class="med_txt ">Diagramme of Risk</div>
+                <div class="big_txt">Management Process</div>
+            </div>
+            <!-- <div class="small_tile tile1">
+                <div class="tile_desc"></div>
+                <img src="" class="tile_img" />
+            </div>
+            <div class="small_tile tile2_1">
+                <div class="tile_desc">Chief Risk <br>Officer</div>
+                <img src="" class="tile_img" />
+            </div>
+            <div class="small_tile tile2_2">
+                <div class="tile_desc">Board Audit<br> Committee</div>
+                <img src="" class="tile_img" />
+            </div>
+            <div class="small_tile tile3_1">
+                <div class="tile_desc">Group Risk <br> Management</div>
+                <img src="" class="tile_img" />
+            </div>
+            <div class="small_tile tile3_2">
+                <div class="tile_desc">Risk Management<br> Committee (RMC)</div>
+                <img src="" class="tile_img" />
+            </div>
+            <div class="small_tile tile4">
+                <div class="tile_desc">Board / <br> Risk Council</div>
+                <img src="" class="tile_img" />
+            </div>
+            <div class="big_tile tile5_1"></div>
+            <div class="med_tile tile5_2"></div>
+            <div class="diagram_foot_note tile6"></div> -->
+        </div>
+        </div>
+    </div>
+
 </div>
 
 <?php
     include("../inc/footer.php");
 ?>
-<link rel="stylesheet" href="../assets/css/ch2/ch2_1.css">
-<script src="../assets/js/ch2/ch2_1.js"></script>
+<link rel="stylesheet" href="../assets/css/risk_management.css">
+<script src="../assets/js/risk_management.js"></script>
 <script type="text/javascript">
     
         
