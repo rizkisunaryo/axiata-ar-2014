37d2e8313927f2f1181b9d0a320c74de18ffb09e
Rizki Sunaryo <rizkisunaryo@gmail.com>
2015-04-28 15:38:06 +0800

    chapter2_3 done

---
+++ ar/performance/chapter2_3.php
@@ -0,0 +1,276 @@
+<?php
+
+    include("../inc/header.php");
+
+?>
+<link rel="stylesheet" href="../assets/css/ch2/chapter2_3.css">
+
+<div id="scrollTrigger90px" style="position:fixed; top:90px; z-index:1000;"></div>
+<div id="scrollTrigger10" style="position:fixed; top:10%; z-index:1000;"></div>
+<div id="scrollTrigger20" style="position:fixed; top:20%; z-index:1000;"></div>
+<div id="scrollTrigger25" style="position:fixed; top:25%; z-index:1000;"></div>
+<div id="scrollTrigger30" style="position:fixed; top:30%; z-index:1000;"></div>
+<div id="scrollTrigger40" style="position:fixed; top:40%; z-index:1000;"></div>
+<div id="scrollTrigger50" style="position:fixed; top:50%; z-index:1000;"></div>
+<div id="scrollTrigger60" style="position:fixed; top:60%; z-index:1000;"></div>
+<div id="scrollTrigger70" style="position:fixed; top:70%; z-index:1000;"></div>
+<div id="scrollTrigger75" style="position:fixed; top:75%; z-index:1000;"></div>
+<div id="scrollTrigger80" style="position:fixed; top:80%; z-index:1000;"></div>
+<div id="scrollTrigger90" style="position:fixed; top:90%; z-index:1000;"></div>
+<div id="scrollTrigger100" style="position:fixed; top:100%; z-index:1000;"></div>
+
+<section id="keyHighlightsSection">
+	<div style="width:960px; margin:0 auto; padding:55px; margin-top:90px; height:1400px; box-sizing:border-box; overflow:hidden;">
+		<div id="keyPerfHeader" style="position:relative; background-color:white; z-index:20; padding-top:100px; top:-100px;">
+      <div style="position:relative;">
+        <div class="title" style="position:relative; font-size:26px; font-color:#8a8c8e;">Key Performance</div>
+        <div class="title maroonColor" style="position:relative; font-size:33px;">Indicators</div>
+        <div style="position:relative; margin-top:20px;">As a Group, Axiata is firm on setting its Key Performance Indicators (KPIs) based on the Group’s strategic direction and vision of being a regional champion. Below is our progress on the KPIs measured and published each year.</div>
+      </div>
+      <div style="position:relative; margin-top:20px;">
+        <table width="890">
+          <col width="320">
+          <col width="250">
+          <col width="320">
+          <tr bgcolor="#c40c42">
+            <td height="36" style="border-right:solid 1px white; padding-left:15px; padding-right:40px;">
+              <div class="title" style="position:relative; font-size:18px; color:white; color:white;">FY2014 KPI</div>
+            </td>
+            <td style="border-right:solid 1px white;"></td>
+            <td>
+              <div class="title maroonColor" style="position:relative; font-size:18px; color:white; color:white; padding-left:30px; padding-right:15px;">Achievements</div>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <div style="position:relative; top:-100px;">
+      <table width="890">
+        <col width="320">
+        <col width="250">
+        <col width="320">
+        <tr>
+          <td height="240" style="border-right:solid 1px; border-bottom:solid 1px; padding-left:15px; padding-right:40px;">
+            <div class="title maroonColor" style="position:relative; font-size:22px;">Revenue Growth</div>
+            <div style="position:relative; margin-top:10px;">Revenue growth measures overall YoY growth of the Group relative to competitors and the industry, excluding all translation impact</div>
+          </td>
+          <td style="border-right:solid 1px; border-bottom:solid 1px;">
+            <div id="graphTriggerBottomRow4" style="position:relative; width:83px; height:6px; border-style:solid; border-top-style:none; border-width:1px; background-color:white; z-index:10; margin:0 auto; top:70px;"></div>
+            <div id="graph41" style="position:absolute; top:180px; left:414px;"></div>
+            <div id="graph42" style="position:absolute; top:180px; left:454px;"></div>
+            <div class="graphRow4Year" style="position:absolute; top:410px; left:420px;">
+              <div style="position:relative; font-size:12px; top:-200px; left:-40px; top:-205px;" class="graphYear">2014 KPI</div>
+              <div class="title graphYear" style="position:relative; font-size:12px; top:-216px; left:0px;">FY2014*</div>
+            </div>
+            <div class="graphRow4Number" style="position:absolute;">
+              <div id="graphTriggerTopRow4" style="position:relative; top:-115px; left:89px;">10.1%</div>
+              <div style="position:relative; top:-35px; left:128px;" class="title">4.4%</div>
+            </div>
+          </td>
+          <td rowspan="2" style="border-bottom:solid 1px; padding-left:30px; padding-right:15px;">
+            <div style="position:relative;">Challenging year for the Group primarily due to the delay of one quarter in Axis acquisition in Indonesia, coupled with challenges faced in the Malaysia operations arising from the IT transformation programme</div>
+          </td>
+        </tr>
+        <tr>
+          <td height="200" style="border-right:solid 1px; border-bottom:solid 1px; padding-left:15px; padding-right:40px;">
+            <div class="title maroonColor" style="position:relative; font-size:22px;">EBITDA Growth</div>
+            <div style="position:relative; margin-top:10px;">EBITDA growth measures overall YoY growth of the Group relative to competitors and the industry, excluding all translation impact</div>
+          </td>
+          <td style="border-right:solid 1px; border-bottom:solid 1px;">
+            <div id="graphTriggerBottomRow5" style="position:relative; width:83px; height:6px; border-style:solid; border-top-style:none; border-width:1px; background-color:white; z-index:10; margin:0 auto; top:10px;"></div>
+            <div id="graph51" style="position:absolute; top:342px; left:414px;"></div>
+            <div id="graph52" style="position:absolute; top:341px; left:454px;"></div>
+            <div class="graphRow5Year" style="position:absolute; top:570px; left:420px;">
+              <div style="position:relative; font-size:12px; top:-200px; left:-40px; top:-205px;" class="graphYear">2014 KPI</div>
+              <div class="title graphYear" style="position:relative; font-size:12px; top:-264px; left:40px;">FY2014*</div>
+            </div>
+            <div class="graphRow5Number" style="position:absolute;">
+              <div id="graphTriggerTopRow5" style="position:relative; top:-90px; left:95px;">1.8%</div>
+              <div style="position:relative; top:25px; left:165px;" class="title">-1.2%</div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td height="240" style="border-right:solid 1px; border-bottom:solid 1px; padding-left:15px; padding-right:40px;">
+            <div class="title maroonColor" style="position:relative; font-size:22px;">ROIC</div>
+            <div style="position:relative; margin-top:10px;">ROIC is defined as EBIT less tax over average invested capital</div>
+          </td>
+          <td style="border-right:solid 1px; border-bottom:solid 1px;">
+            <div id="graphTriggerBottomRow6" style="position:relative; width:83px; height:6px; border-style:solid; border-top-style:none; border-width:1px; background-color:white; z-index:10; margin:0 auto; top:70px;"></div>
+            <div id="graph61" style="position:absolute; top:623px; left:414px;"></div>
+            <div id="graph62" style="position:absolute; top:623px; left:454px;"></div>
+            <div class="graphRow6Year" style="position:absolute; top:850px; left:420px;">
+              <div style="position:relative; font-size:12px; top:-200px; left:-40px; top:-205px;" class="graphYear">2014 KPI</div>
+              <div class="title graphYear" style="position:relative; font-size:12px; top:-215px; left:5px;">FY2014*</div>
+            </div>
+            <div class="graphRow6Number" style="position:absolute;">
+              <div id="graphTriggerTopRow6" style="position:relative; top:-80px; left:90px;">9.3%</div>
+              <div style="position:relative; top:-85px; left:130px;" class="title">8.9%</div>
+            </div>
+          </td>
+          <td style="border-bottom:solid 1px;">
+            <div style="position:relative; padding-left:30px; padding-right:15px;">The Group fell short of FY2014 target by 0.4% percentage points</div>
+          </td>
+        </tr>
+        <tr>
+          <td height="250" style="border-right:solid 1px; border-bottom:solid 1px; padding-left:15px; padding-right:40px;">
+            <div class="title maroonColor" style="position:relative; font-size:22px;">ROCE</div>
+            <div style="position:relative; margin-top:10px;">ROCE is defined as EBIT less tax over average capital employed</div>
+          </td>
+          <td style="border-right:solid 1px; border-bottom:solid 1px;">
+            <div id="graphTriggerBottomRow7" style="position:relative; width:83px; height:7px; border-style:solid; border-top-style:none; border-width:1px; background-color:white; z-index:10; margin:0 auto; top:70px;"></div>
+            <div id="graph71" style="position:absolute; top:868px; left:414px;"></div>
+            <div id="graph72" style="position:absolute; top:868px; left:454px;"></div>
+            <div class="graphRow7Year" style="position:absolute; top:850px; left:420px;">
+              <div style="position:relative; font-size:12px; left:-40px; top:40px;" class="graphYear">2014 KPI</div>
+              <div class="title graphYear" style="position:relative; font-size:12px; top:30px; left:5px;">FY2014*</div>
+            </div>
+            <div class="graphRow7Number" style="position:absolute;">
+              <div id="graphTriggerTopRow7" style="position:relative; top:-115px; left:90px;">7.8%</div>
+              <div style="position:relative; top:-120px; left:130px;" class="title">7.5%</div>
+            </div>
+          </td>
+          <td style="border-bottom:solid 1px;">
+            <div style="position:relative; padding-left:30px; padding-right:15px;">The Group fell short of FY2014 target by 0.3% percentage points</div>
+          </td>
+        </tr>
+      </table>
+    </div>
+	</div>
+</section>
+
+<?php
+    include("../inc/footer.php");
+?>
+
+<script type="text/javascript">
+	var graphAnimationTime = 1000;
+	var bottomTrigger = $('#scrollTrigger100');
+	var isGraphRow4Reached = [false,false];
+	var isGraphRow5Reached = [false,false];
+	var isGraphRow6Reached = [false,false];
+	var isGraphRow7Reached = [false,false];
+	var graph4ToFunctionArray = [];
+	var graph5ToFunctionArray = [];
+	var graph6ToFunctionArray = [];
+	var graph7ToFunctionArray = [];
+
+	$(function () {
+		callAllGraphAnimations();
+		$(window).on('scroll', function(){
+			callAllGraphAnimations();
+		});
+	});
+
+	function callAllGraphAnimations() {
+		animateGraphRowCall(graph4ToFunctionArray,isGraphRow4Reached,4,1,true,"Grey",1.0,13);
+		animateGraphRowCall(graph4ToFunctionArray,isGraphRow4Reached,4,2,true,"Red",1.0,5);
+
+		animateGraphRowCall(graph5ToFunctionArray,isGraphRow5Reached,5,1,true,"Grey",1.0,6);
+		animateGraphRowCall(graph5ToFunctionArray,isGraphRow5Reached,5,2,false,"Red",1.0,4);
+
+		animateGraphRowCall(graph6ToFunctionArray,isGraphRow6Reached,6,1,true,"Grey",1.0,10);
+		animateGraphRowCall(graph6ToFunctionArray,isGraphRow6Reached,6,2,true,"Red",1.0,9);
+
+		animateGraphRowCall(graph7ToFunctionArray,isGraphRow7Reached,7,1,true,"Grey",1.0,13);
+		animateGraphRowCall(graph7ToFunctionArray,isGraphRow7Reached,7,2,true,"Red",1.0,12);
+	}
+
+	function animateGraphRowCall(graphToFuncArray, graphReachedBool,row,column,isGoingUp,color,opacity,height) {
+		var bottomTriggerTop = bottomTrigger.offset().top;
+		var bottomGraphTriggerTop = $('#graphTriggerBottomRow'+row).offset().top;
+		var topGraphTriggerTop = $('#graphTriggerTopRow'+row).offset().top;
+		var selector = $('#graph'+row+''+column);
+
+		// console.log(bottomTriggerTop+" : "+topGraphTriggerTop+" : "+bottomGraphTriggerTop+" : "+(bottomTriggerTop>bottomGraphTriggerTop)+" : "+(!graphReachedBool[column-1]));
+
+    if (bottomTriggerTop>bottomGraphTriggerTop && !graphReachedBool[column-1]) {
+      graphReachedBool[column-1]=true;
+      animateGraph(row,selector,isGoingUp,color,opacity,height);
+    }
+    else if (bottomTriggerTop<topGraphTriggerTop) {
+      graphReachedBool[column-1]=false;
+      $(selector).html('');
+      graphToFuncArray.forEach(function(entry) {
+        clearTimeout(entry);
+      });
+    }
+	}
+
+	function animateGraph(rowNumber,selector,isGoingUp,color,pOpacity,height) {
+		var opacityDecreasor=0.25;
+		if (color=='Red') opacityDecreasor=0.1;
+    var facingLeft = false;
+    var facing = '';
+    var top=6;
+    if (!isGoingUp) top=0;
+    var left=0;
+    var opacity=1.0*pOpacity;
+    
+    for (i1=1;i1<=height*4;i1++) {
+      if (i1%2==0) {
+          facingLeft=!facingLeft;
+          left=12;
+      } else {
+          if (isGoingUp) top-=6;
+          else top+=6;
+          left=0;
+      }
+
+      if (i1%3==0) {
+          opacity=1.1;
+      }
+      opacity-=opacityDecreasor;
+      opacity*=pOpacity;
+
+      if (facingLeft) {
+          facing="Left";
+      } else {
+          facing="Right";
+      }
+
+      var html='<canvas class="triangle'+facing+color+'" style="top:'+top+'px; left:'+left+'px; opacity:'+opacity+';"></canvas>';
+      animateGraphAction(rowNumber,selector,html,i1,height*4);
+    }
+	}
+
+	function animateGraphAction(rowNumber,selector,html,i,blockCount) {
+    var graphToFunction = setTimeout(function(){ 
+      $(selector).append(html);
+    }, graphAnimationTime/blockCount*i);
+
+    addGraphToArray(rowNumber,graphToFunction);
+	}
+
+	function showGraphNumberAndYear(rowNumber,selector,delay) {
+    var graphInfo = setTimeout(function(){ 
+      $(selector).fadeTo("slow", 1.0);
+    }, delay);
+
+    addGraphToArray(rowNumber,graphInfo);
+	}
+
+	function addGraphToArray(rowNumber,functionName) {
+    if (rowNumber==1) {
+      graph1ToFunctionArray.push(functionName);
+    }
+    else if (rowNumber==2) {
+      graph2ToFunctionArray.push(functionName);
+    }
+    else if (rowNumber==3) {
+      graph3ToFunctionArray.push(functionName);
+    }
+    else if (rowNumber==4) {
+      graph4ToFunctionArray.push(functionName);
+    }
+    else if (rowNumber==5) {
+      graph5ToFunctionArray.push(functionName);
+    }
+    else if (rowNumber==6) {
+      graph6ToFunctionArray.push(functionName);
+    }
+    else if (rowNumber==7) {
+      graph7ToFunctionArray.push(functionName);
+    }
+	}
+</script>
