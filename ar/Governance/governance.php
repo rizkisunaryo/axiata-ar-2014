4d559297453615d4783dc9679e9507298fc005b8
Abidul <abd_rmdn@yahoo.com>
2015-04-30 22:01:37 +0800

    fixing latest bugs]

---
+++ ar/Governance/governance.php
@@ -0,0 +1,68 @@
+<?php
+    include("../inc/header.php");
+?>
+<style>
+    .paragrapg_x{
+        color: #4d4d4f;
+        font-size:15px;
+        font-family:AxiataBook;
+        margin-bottom:20px;
+        text-align: justify;
+    text-justify: inter-word;
+    }
+    .title_s{
+        font-size:25px;
+        color:#c40d42;
+        font-familly: AxiataBold;
+    }
+    .grey_txt{
+        color:#8a8c8e;
+        display: inline-block;
+    }
+</style>	
+
+<div idclass="spacer s0"></div>
+
+<div id="movingSectionContainer" class="zoomViewport" >
+    
+    <div class="section1 section " style="display:none;">
+        <img src="../assets/img/merged_governance.png"/>
+         <div class="paragrapg_x">
+              
+             <div class="title_s"><div class="grey_txt">Code of</div> Conduct</div><br>
+            Axiata is committed to conduct its business fairly, impartially and in full compliance with all applicable laws and regulations in Malaysia and in countries where the Group has operations. The Group's professionalism, honesty and integrity must at all times be upheld in all of the Company's business dealings by all employees.<br>
+<br>
+            Axiata has in place a Code of Conduct that serves as documentation of its commitment in ensuring its business dealings are conducted in a manner that is efficient, effective and fair. Axiata ensures that it is the responsibility of every employee to act in accordance with the policies detailed in the Group's Code of Conduct.
+             <div class="title_s"><br><br><div class="grey_txt">Core</div> Values</div>
+            At Axiata, there are two core values that we embrace across the Group: Uncompromising Integrity and Exceptional Performance. These two values define who we are and how we operate.
+            <br><br><br>
+        </div>
+        <img src="../assets/img/governance_middle_img.png"/>
+        <div class="paragrapg_x"> 
+        <br><br><br>
+            Uncompromising Integrity and Exceptional Performance are our distinct values and key to our success towards becoming a regional champion. 
+<br><br>
+            We place great emphasis in building a culture based on these two values across the Group. 
+<br><br>
+            These two core values will be incorporated into the existing values of the individual Operating Companies (OpCo) which includes, amongst others, service excellence, teamwork, creativity and customer centricity.
+            <br><br><br>
+        </div>
+    </div>
+    
+</div>
+
+
+<?php
+    include("../inc/footer.php");
+?>
+<script>
+    
+    $(document).ready(function(){
+        
+            
+    });
+    
+</script>
+<link rel="stylesheet" href="../assets/css/chapter2_6.css">
+<script src="../assets/js/ch2/chapter2_6.js"></script>
+<script src="../assets/js/jquery.zoomooz.min.js"></script>
