4b5a29bab8ecd4e732e8ff51ca8478ebe9eacc73
Abidul <abd_rmdn@yahoo.com>
2015-05-08 01:42:10 +0800

    fixing style in 2_6

--- ar/performance/chapter2_6.php
+++ ar/performance/chapter2_6.php
@@ -309,10 +309,18 @@
 <script>
     
     $(document).ready(function(){
-    if (navigator.userAgent.indexOf('Safari') != -1 )
-    {
-        $(".graph_num").addClass("safari");
-    }    
+        var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+        // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+        var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+        var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+            // At least Safari 3+: "[object HTMLElementConstructor]"
+        var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+        var isIE = /*@cc_on!@*/false || !!document.documentMode;   // At least IE6
+        
+        if (isSafari)
+        {
+            $(".graph_num").addClass("safari");
+        }    
             
     });
     
