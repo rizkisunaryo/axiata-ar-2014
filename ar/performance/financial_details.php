5cd305b0b3b8cd3f8c66874af7b7ac7fa75c80e3
Abidul <abd_rmdn@yahoo.com>
2015-05-08 15:59:22 +0800

    adding the new financial_details page

---
+++ ar/performance/financial_details.php
@@ -0,0 +1,25 @@
+<?php
+
+    include("../inc/header.php");
+
+?>
+<link rel="stylesheet" href="../assets/css/financial_details.css">
+<style type="text/css">
+	.black_txt{
+        color:#524e4c;
+	}
+</style>
+<div class="section">
+    <div class="title_text bold black_txt" style="color:">
+        To read more about our financial details and financial statement,<br>
+        click the buttons below
+    </div>
+	<div class="tiles">
+		<div class="tile"><a href="../assets/pdfs/financial_details/Axiata2014 AR_Five-Year Group Financial Summary.pdf" target="_blank"><img src="../assets/img/financial_detals_tile1.png"></a></div>
+		<div class="tile"><a href="../assets/pdfs/financial_details/Axiata2014 AR_Group Financial Analysis.pdf" target="_blank"><img src="../assets/img/financial_detals_tile2.png"></a></div>
+		<div class="tile"><a href="../assets/pdfs/financial_details/Axiata2014 AR_Financial Statement.pdf" target="_blank"><img src="../assets/img/financial_detals_tile3.png"></a></div>
+	</div>
+</div>
+<?php
+    include("../inc/footer.php");
+?>
