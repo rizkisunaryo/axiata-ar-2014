20b4cc718900f59c1e47bf6c5176c961c69aea7b
Abidul <abd_rmdn@yahoo.com>
2015-05-08 16:38:22 +0800

    ch1_1

--- sr/Advancing_Asia/Advancing_Asia.php
+++ sr/Advancing_Asia/Advancing_Asia.php
@@ -49,46 +49,46 @@
         <div style="position:absolute; top:0; left:0;">
           <div id="trigger" class="beacon"  style="top:250px; left:285px;"></div>
           <div id="trigger2" style="top:595px; left:47px;"></div>
-          <div id="sibling" style="position:absolute; top:147px; left:300px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-bangladesh.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:147px; left:300px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-bangladesh.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:344px; left:386px;" id="trigger" class="beacon" ></div>
           <div style="top:516px; left:202px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:246px; left:396px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-cambodia.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:246px; left:396px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-cambodia.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:595px; left:443px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:340px; left:250px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-eco.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:340px; left:250px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-eco.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:276px; left:189px;" id="trigger" class="beacon" ></div>
           <div style="top:441px; left:49px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:193px; left:200px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-india.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:193px; left:200px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-india.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:525px; left:449px;" id="trigger" class="beacon" ></div>
           <div style="top:594px; left:344px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:319px; left:460px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-indonesia.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:319px; left:460px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-indonesia.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:429px; left:382px;" id="trigger" class="beacon" ></div>
           <div style="top:595px; left:202px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:260px; left:395px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-malaysia.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:260px; left:395px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-malaysia.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:206px; left:68px;" id="trigger" class="beacon" ></div>
           <div style="top:365px; left:49px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:115px; left:80px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-pakistan.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:115px; left:80px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-pakistan.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:454px; left:403px;" id="trigger" class="beacon" ></div>
           <div style="top:516px; left:330px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:368px; left:416px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-singapore.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:368px; left:416px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-singapore.png"></div>
         </div>
         <div style="position:absolute; top:0; left:0;">
           <div style="top:403px; left:205px;" id="trigger" class="beacon" ></div>
           <div style="top:517px; left:48px;" id="trigger2"></div>
-          <div id="sibling" style="position:absolute; top:304px; left:217px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-sri-lanka.png"></div>
+          <div id="sibling" style="position:absolute;   z-index: 999; top:304px; left:217px;"><img src="../assets/img/Advancing_Asia/Advancing_Asia/pop-up-_-sri-lanka.png"></div>
         </div>
 			</div>
 		</section>
